55df06d0eb13a05e78faa00d2611fc674049bc24
DMistica <hildtgunnt@gmail.com>
2023-11-02 09:04:27 +0000

    First push

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+const axios = require('axios');
+
+// add your endpoints here
+
+app.get(PORT, (request, response) => response.json("Root route for translation"))
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
